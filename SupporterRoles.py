9d1065fc05edcc427d768d5c13eee7bd769fbfc7
Andrew McNeill <andrewjndmcneill@gmail.com>
2021-06-18 04:02:38 -0400

    Moved supporter role settings to json

--- SupporterRoles.py
+++ SupporterRoles.py
@@ -5,7 +5,6 @@ from discord.guild import Guild
 from discord.member import Member
 from discord.role import Role
 
-GUILD_ID = 134626324256915456
 SUPPORTER_ROLES = [854637983370182676, 854657267479871498] # Twitch/Patreon
 SERVER_SUPPORTER = 854638052152311808 # Server supporter
 
@@ -16,26 +15,27 @@ class SupporterRolesCog(commands.Cog):
 
         self.test_supporter.start()
         
-    async def test(member: Member):
-        if any(role.id in SUPPORTER_ROLES for role in member.roles):
-            if not any(role.id == SERVER_SUPPORTER for role in member.roles):
-                await member.add_roles(discord.utils.get(member.guild.roles, id=SERVER_SUPPORTER))
-                print(f"Added supporter role to {member.display_name}")
-        elif any(role.id == SERVER_SUPPORTER for role in member.roles):
-            await member.remove_roles(discord.utils.get(member.guild.roles, id=SERVER_SUPPORTER))
-            print(f"Removed supporter role from {member.display_name}")
+    async def test(self, member: Member):
+        for supporter_role in self.data['supporter_roles']:
+            main_role = supporter_role['main_role']
+            sub_roles = supporter_role['sub_roles']
+            
+            if any(role.id in sub_roles for role in member.roles):
+                if not any(role.id == main_role for role in member.roles):
+                    await member.add_roles(discord.utils.get(member.guild.roles, id=main_role))
+            elif any(role.id == main_role for role in member.roles):
+                await member.remove_roles(discord.utils.get(member.guild.roles, id=main_role))
 
     @tasks.loop(minutes=1)
     async def test_supporter(self):
         guild: Guild
         for guild in self.bot.guilds:
-            # TODO: Use Giana's server ID
             if guild.id != self.data["server_id"]: continue
 
             member: Member
             for member in guild.members:
                 role: Role
-                await SupporterRolesCog.test(member)
+                await self.test(member)
     
     @test_supporter.before_loop
     async def before_test_supporter(self):
@@ -44,4 +44,4 @@ class SupporterRolesCog(commands.Cog):
     @commands.Cog.listener()
     async def on_member_update(self, before: Member, after: Member):
         if after.guild.id == self.data["server_id"]:
-            await SupporterRolesCog.test(after)
+            await self.test(after)
