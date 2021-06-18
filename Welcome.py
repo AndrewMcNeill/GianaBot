cf419786d05c1b72b1ad01616b2cc1baf87152b8
Andrew McNeill <andrewjndmcneill@gmail.com>
2021-06-18 10:36:58 -0400

    Made welcome messages optional

--- Welcome.py
+++ Welcome.py
@@ -8,9 +8,11 @@ from discord.member import Member
 class WelcomeCog(commands.Cog):
     def __init__(self, bot: commands.Bot, json_data):
         self.bot = bot
-        self.data = json_data["welcome"]
+        self.data = json_data['welcome']
 
     @commands.Cog.listener()
     async def on_member_join(self, member: Member):
-        await self.bot.get_channel(self.data["channel"]).send(random.choice(self.data["messages"]).format(member.display_name))
+        if len(self.data['messages']) == 0: return
+        message = random.choice(self.data['messages'])
+        await self.bot.get_channel(self.data['channel']).send(message.format(member.display_name))
         pass
